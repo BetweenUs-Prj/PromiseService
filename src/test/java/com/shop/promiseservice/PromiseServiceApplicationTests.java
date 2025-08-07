6873587640da43629775236ac6e5b32e3ec0a620
cheeze37 <cheeze37@hanmail.net>
2025-08-07 13:47:10 +0900

    Initial commit

---
+++ src/test/java/com/shop/promiseservice/PromiseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.promiseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromiseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
